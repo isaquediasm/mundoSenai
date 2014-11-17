ba67d3413a8f2ba712540937f3acb2bac7b6d647
Isaque Dias <isaquediasm@gmail.com>
2014-11-17 15:48:55 -0200

    3º passo

--- www/js/app.js
+++ www/js/app.js
@@ -2,11 +2,19 @@ angular.module('cardapio', ['ionic'])
 
 .controller('MainCtrl', function($scope){
   $scope.pizzas = 
-  {
+  [{
     id: 1, 
     sabor: 'Atum', 
     valor: 33.99, 
-    descricao: 'Lorem Ipsum'
-  }
+    descricao: 'Lorem Ipsum',
+    img: 'img/pizza-1.jpg'
+  },
+  {
+    id: 2, 
+    sabor: '4 Pizzas', 
+    valor: 32.99, 
+    descricao: 'Lorem Ipsum',
+    img: 'img/pizza-2.jpg'
+  }];
   
 })
