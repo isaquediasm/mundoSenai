2e48b4da3efc165d737c9996359fdf1ca71f5a34
Isaque Dias <isaquediasm@gmail.com>
2014-11-17 14:59:37 -0200

    2º Passo

--- www/js/app.js
+++ www/js/app.js
@@ -1,1 +1,12 @@
 angular.module('cardapio', ['ionic'])
+
+.controller('MainCtrl', function($scope){
+  $scope.pizzas = 
+  {
+    id: 1, 
+    sabor: 'Atum', 
+    valor: 33.99, 
+    descricao: 'Lorem Ipsum'
+  }
+  
+})
