5ab3dbe1d753cab54100b124c5bf18751f7cf0a3
Isaque Dias <isaquediasm@gmail.com>
2014-11-21 02:30:52 -0200

    12º Passo - Bebidas

--- www/js/app.js
+++ www/js/app.js
@@ -51,9 +51,122 @@ angular.module('cardapio', ['ionic'])
         comentario: 'Essa pizza é boa pakas, mano!'
       }
     ]
-  }];
-  
+  },
+  {
+    id: 2, 
+    sabor: 'Calabresa', 
+    valor: 32.99, 
+    descricao: 'Lorem Ipsum',
+    img: 'img/pizza-3.jpg',
+    nota: 0,
+    comentarios: 
+    [
+      {
+        autor: 'Isaque Dias',
+        email: 'isaquediasm@gmail.com',
+        comentario: 'Essa pizza é boa pakas, mano!'
+      }
+    ]
+  },
+  {
+    id: 3, 
+    sabor: 'Portuguesa', 
+    valor: 32.99, 
+    descricao: 'Lorem Ipsum',
+    img: 'img/pizza-4.jpg',
+    nota: 0,
+    comentarios: 
+    [
+      {
+        autor: 'Isaque Dias',
+        email: 'isaquediasm@gmail.com',
+        comentario: 'Essa pizza é boa pakas, mano!'
+      }
+    ]
+  },
+  {
+    id: 4, 
+    sabor: 'Sertaneja', 
+    valor: 32.99, 
+    descricao: 'Lorem Ipsum',
+    img: 'img/pizza-5.jpg',
+    nota: 0,
+    comentarios: 
+    [
+      {
+        autor: 'Isaque Dias',
+        email: 'isaquediasm@gmail.com',
+        comentario: 'Essa pizza é boa pakas, mano!'
+      }
+    ]
+  },
+  {
+    id: 5, 
+    sabor: 'Baidu', 
+    valor: 32.99, 
+    descricao: 'Lorem Ipsum',
+    img: 'img/pizza-6.jpg',
+    nota: 0,
+    comentarios: 
+    [
+      {
+        autor: 'Isaque Dias',
+        email: 'isaquediasm@gmail.com',
+        comentario: 'Essa pizza é boa pakas, mano!'
+      }
+    ]
+  },
+  {
+    id: 6, 
+    sabor: 'Avast', 
+    valor: 32.99, 
+    descricao: 'Lorem Ipsum',
+    img: 'img/pizza-7.jpg',
+    nota: 0,
+    comentarios: 
+    [
+      {
+        autor: 'Isaque Dias',
+        email: 'isaquediasm@gmail.com',
+        comentario: 'Essa pizza é boa pakas, mano!'
+      }
+    ]
+  },
+  {
+    id: 7, 
+    sabor: 'AVG', 
+    valor: 32.99, 
+    descricao: 'Lorem Ipsum',
+    img: 'img/pizza-8.jpg',
+    nota: 0,
+    comentarios: 
+    [
+      {
+        autor: 'Isaque Dias',
+        email: 'isaquediasm@gmail.com',
+        comentario: 'Essa pizza é boa pakas, mano!'
+      }
+    ]
+  },
+  {
+    id: 8, 
+    sabor: 'Avira', 
+    valor: 32.99, 
+    descricao: 'Lorem Ipsum',
+    img: 'img/pizza-9.jpg',
+    nota: 0,
+    comentarios: 
+    [
+      {
+        autor: 'Isaque Dias',
+        email: 'isaquediasm@gmail.com',
+        comentario: 'Essa pizza é boa pakas, mano!'
+      }
+    ]
+  }
 
+  ];
+  
   return {
     all: function() {
       return pizzas;
@@ -70,9 +183,57 @@ angular.module('cardapio', ['ionic'])
   }
 })
 
-.controller('MainCtrl', function($scope, $ionicSideMenuDelegate, $ionicSlideBoxDelegate, $ionicNavBarDelegate, PizzasFactory){
+.factory('BebidasFactory', function() {
+
+  var bebidas = [{
+    id: 0, 
+    nome: 'Coca-Cola', 
+    valor: 33.99, 
+    descricao: 'Lorem Ipsum',
+    img: 'img/bebida-coca.jpg'
+  },
+  {
+    id: 1, 
+    nome: 'Cerveja', 
+    valor: 32.99, 
+    descricao: 'Lorem Ipsum',
+    img: 'img/bebida-cerveja.jpg'
+  },
+  {
+    id: 2, 
+    nome: 'Suco', 
+    valor: 32.99, 
+    descricao: 'Lorem Ipsum',
+    img: 'img/bebida-suco.jpg'
+  },
+  {
+    id: 3, 
+    nome: 'Champagne', 
+    valor: 32.99, 
+    descricao: 'Lorem Ipsum',
+    img: 'img/bebida-champagne.jpg'
+  },
+  {
+    id: 4, 
+    nome: 'Vinho', 
+    valor: 32.99, 
+    descricao: 'Lorem Ipsum',
+    img: 'img/bebida-vinho.jpg'
+  }
+  ];
+  
+
+  return {
+    all: function() {
+      return bebidas;
+    }
+  }
+})
+
+.controller('MainCtrl', function($scope, $ionicSideMenuDelegate, $ionicSlideBoxDelegate, $ionicNavBarDelegate, PizzasFactory, BebidasFactory){
   
   $scope.pizzas = PizzasFactory.all();
+  $scope.bebidas = BebidasFactory.all();
   
   /* 
       Funções responsáveis pela manipulação do slide do ionic.
