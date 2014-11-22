3c66e434d57b5f078c23cd48ee4a4c88b9907068
Isaque Dias <isaquediasm@gmail.com>
2014-11-22 12:33:36 -0200

    Final

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,50 +1,13 @@
-angular.module('app.controllers', [])
+angular.module('starter.controllers', [])
 
-.controller('MainCtrl', function($scope , $ionicSideMenuDelegate, $ionicSlideBoxDelegate, $ionicNavBarDelegate, Pizzas) {
-   $scope.next = function() {
-    $ionicSlideBoxDelegate.next();
-  };
-  $scope.previous = function() {
-    $ionicSlideBoxDelegate.previous();
-  };
-
-  // Called each time the slide changes
-  $scope.slideChanged = function(index) {
-    $scope.slideIndex = index;
-  };
- $scope.goBack = function() {
-    $ionicNavBarDelegate.back();
-  };
-
-  $scope.pizzas = Pizzas.all();
-
-  
+.controller('DashCtrl', function($scope, Pizzas) {
+	$scope.pizzas = Pizzas.all();
 })
 
-.controller('PizzasCtrl', function($scope, $stateParams, $ionicNavBarDelegate, Pizzas, Utils) {
+.controller('PizzaDetalheCtrl', function($scope, $stateParams, Pizzas) {
+	
+})
 
-  var hoje = new Date();
-  var dia = hoje.getDate();
-  var mes = hoje.getMonth()+1;
-  var ano = hoje.getFullYear();
-  var dataFinal = dia + "/" + mes + "/" + ano;
-
-
-  $scope.pizzaId = $stateParams.pizzaId;
-  $scope.pizza = Pizzas.get($scope.pizzaId);
-
-   $scope.avaliarPizza = function(valor) {
-    $scope.pizza.nota = valor;
-  }
-
-  $scope.coment = {
-    data: dataFinal
-  };
-
-  $scope.cadastraComent = function(item) {
-    Utils.showLoading();
-    $scope.pizza.comentarios.push(item);
-    Utils.hideLoading();
-    $scope.coment = "";
-  }
-});
+.controller('BebidasCtrl', function($scope, Bebidas) {
+	$scope.bebidas = Bebidas.all();
+})
--- www/js/services.js
+++ www/js/services.js
@@ -1,112 +1,17 @@
-angular.module('app.services', [])
+angular.module('starter.services', [])
 
-.factory('Pizzas', function($http) {
+/**
+ * A simple example service that returns some data.
+ */
+.factory('Pizzas', function() {
   // Might use a resource here that returns a JSON array
 
   // Some fake testing data
   var pizzas = [
-    { 
-      id: 1, 
-      sabor: 'Caipira', 
-      valor: 33.99, 
-      img: 'pizza-1.jpg', 
-      descricao: 'Lorem Ipsum', 
-      indisponivel: false, 
-      nota: 0,
-      comentarios: [
-        {
-          id: 1,
-          autor: 'Isaque Dias',
-          email: 'isaquediasm@gmail.com',
-          data: '14/11/2014',
-          comentario: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmodtempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodoconsequat.'
-        },
-        {
-          id: 2,
-          autor: 'Filipe Dias',
-          email: 'filipediasm@gmail.com',
-          data: '14/11/2014',
-          comentario: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmodtempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodoconsequat.'
-        }
-      ]
-    },
-    { 
-      id: 2, 
-      sabor: 'Calabresa', 
-      valor: 33.99, 
-      img: 'pizza-2.jpg', 
-      descricao: 'Lorem Ipsum', 
-      indisponivel: false, 
-      nota: 0,
-      comentarios: [
-        {
-          id: 1,
-          autor: 'Isaque Dias',
-          email: 'isaquediasm@gmail.com',
-          data: '14/11/2014',
-          comentario: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmodtempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodoconsequat.'
-        },
-        {
-          id: 2,
-          autor: 'Filipe Dias',
-          email: 'filipediasm@gmail.com',
-          data: '14/11/2014',
-          comentario: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmodtempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodoconsequat.'
-        }
-      ]
-    },
-    { 
-      id: 3, 
-      sabor: 'Atum', 
-      valor: 33.99, 
-      img: 'pizza-3.jpg', 
-      descricao: 'Lorem Ipsum', 
-      indisponivel: false, 
-      nota: 0,
-      comentarios: [
-        {
-          id: 1,
-          autor: 'Isaque Dias',
-          email: 'isaquediasm@gmail.com',
-          data: '14/11/2014',
-          comentario: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmodtempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodoconsequat.'
-        },
-        {
-          id: 2,
-          autor: 'Filipe Dias',
-          email: 'filipediasm@gmail.com',
-          data: '14/11/2014',
-          comentario: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmodtempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodoconsequat.'
-        }
-      ]
-    },
-    { 
-      id: 4, 
-      sabor: '4 Queijos', 
-      valor: 33.99, 
-      img: 'pizza-4.jpg', 
-      descricao: 'Lorem Ipsum', 
-      indisponivel: false, 
-      nota: 0,
-      comentarios: 
-      [
-        {
-          id: 1,
-          autor: 'Isaque Dias',
-          email: 'isaquediasm@gmail.com',
-          data: '14/11/2014',
-          comentario: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmodtempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodoconsequat.'
-          
-        },
-        {
-          id: 2,
-          autor: 'Filipe Dias',
-          email: 'filipediasm@gmail.com',
-          data: '14/11/2014',
-          comentario: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmodtempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodoconsequat.'
-        }
-      ]
-    }
+    { id: 0, sabor: 'Caipira', valor: 33.99, img: 'pizza-1.jpg', descricao: 'Lorem Ipsum', indisponivel: false},
+    { id: 1, sabor: 'Caipira', valor: 33.99, img: 'pizza-2.jpg', descricao: 'Lorem Ipsum', indisponivel: false},
+    { id: 2, sabor: 'Caipira', valor: 33.99, img: 'pizza-3.jpg', descricao: 'Lorem Ipsum', indisponivel: false},
+    { id: 3, sabor: 'Caipira', valor: 33.99, img: 'pizza-4.jpg', descricao: 'Lorem Ipsum', indisponivel: false}
   ];
 
   return {
@@ -115,12 +20,7 @@ angular.module('app.services', [])
     },
     get: function(pizzaId) {
       // Simple index lookup
-      for (var i=0;i<=pizzas.length;i++) {
-        if (pizzas[i].id == pizzaId) {
-          return pizzas[i];
-        }
-      }
-      return false;
+      return pizzas[pizzaId];
     }
   }
 })
@@ -147,29 +47,4 @@ angular.module('app.services', [])
   }
 })
 
-.factory('Utils', function($ionicLoading, $timeout) {
 
-  return {
-    // Método para mostrar o ícone de carregamento.
-    showLoading: function() {
-      this.loadingIndicator = $ionicLoading.show({
-      content: '<i class="icon ion-ios7-reloading"></i>',
-      animation: 'fade-in',
-      showBackdrop: true,
-      maxWidth: 200,
-      showDelay: 5  // If the delay is too fast and you also change states, while the loader is showing, you can get flashing behavior
-      }); 
-    },
-
-    hideLoading: function() {
-      // Hide the loadingIndicator 1500 ms later
-      $timeout(function(){
-        $ionicLoading.hide();
-      }, 500); 
-    }
-  }
-
-  
-
- 
-})
